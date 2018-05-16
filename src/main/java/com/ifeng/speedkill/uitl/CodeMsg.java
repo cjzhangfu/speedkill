777650fc6e40f9baa32f3b1b7728374fd2629e2c
zhangfu <zhangfu@mochasoft.com.cn>
2018-05-17 00:20:25 +0800

    result

--- src/main/java/com/ifeng/speedkill/uitl/CodeMsg.java
+++ src/main/java/com/ifeng/speedkill/uitl/CodeMsg.java
@@ -1,5 +1,28 @@
 package com.ifeng.speedkill.uitl;
 
-public class CodeMsg {
+import com.sun.org.apache.bcel.internal.classfile.Code;
 
+public class CodeMsg {
+    private int code;
+    private String msg;
+    public static CodeMsg SUCCESS=new CodeMsg(0,"success");
+    public static CodeMsg SERVER_ERROR = new CodeMsg(500,"服务器异常");
+    public CodeMsg(int code ,String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/com/ifeng/speedkill/uitl/Result.java
+++ src/main/java/com/ifeng/speedkill/uitl/Result.java
@@ -4,8 +4,29 @@ public class Result<T> {
     private int code;
     private String msg;
     private T data;
-    public Result(){
 
+    /**
+     * 成功时返回
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T>Result<T> success(T data){
+        return new Result<T>(data);
+    }
+    public static <T>Result<T> error(CodeMsg msg){
+        return new Result<T>(msg);
+    }
+    public Result(T data){
+        this.code=0;
+        this.msg="success";
+        this.data=data;
+    }
+    public Result(CodeMsg msg){
+        if(msg==null)
+            return;
+        this.code=msg.getCode();
+        this.msg=msg.getMsg();
     }
     public int getCode() {
         return code;
