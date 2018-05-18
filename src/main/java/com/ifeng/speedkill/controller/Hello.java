9956f9e4584d800331d126aa1787bf0ad6d60438
unknown <zhangfu@staff.ifeng.com>
2018-05-18 09:19:38 +0800

    mybatis

--- src/main/java/com/ifeng/speedkill/controller/Hello.java
+++ src/main/java/com/ifeng/speedkill/controller/Hello.java
@@ -1,15 +1,22 @@
 package com.ifeng.speedkill.controller;
 
+import com.ifeng.speedkill.uitl.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/demo")
 public class Hello {
     @RequestMapping("/demo")
-    public  String hello(Model model){
-        model.addAttribute("zhangfu","zhangfu");
+    @ResponseBody
+    public  Result<String> hello(){
+      return Result.success("hello,imooc");
+    }
+    @RequestMapping("/test")
+    public String test(Model model){
+        model.addAttribute("name","zhangfu");
         return "hello";
     }
 }
--- src/main/java/com/ifeng/speedkill/uitl/Result.java
+++ src/main/java/com/ifeng/speedkill/uitl/Result.java
@@ -17,12 +17,12 @@ public class Result<T> {
     public static <T>Result<T> error(CodeMsg msg){
         return new Result<T>(msg);
     }
-    public Result(T data){
+    private Result(T data){
         this.code=0;
         this.msg="success";
         this.data=data;
     }
-    public Result(CodeMsg msg){
+    private Result(CodeMsg msg){
         if(msg==null)
             return;
         this.code=msg.getCode();
@@ -31,24 +31,11 @@ public class Result<T> {
     public int getCode() {
         return code;
     }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
     public String getMsg() {
         return msg;
     }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
     public T getData() {
         return data;
     }
 
-    public void setData(T data) {
-        this.data = data;
-    }
 }
